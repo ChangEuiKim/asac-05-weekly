11cd4847116c65d2c169e7ac7d36e430c29fbbc2
ChangEuiKim <cekim.dev@gmail.com>
2024-03-31 11:51:51 +0900

    fix: 사용자 입력값에 대한 후처리 추가
    - Validate 클래스를 작성하는 과정에서 문자열로 들어온 숫자를 잘못 검증하는 오류를 발견하였다.
    - 이를 해결하기 위해 Input 클래스에서 입력값을 받을때 숫자로 형변환하는 후처리를 추가하였다.
    - Input 클래스에 대한 주석을 추가하였다.

--- src/views/Input.js
+++ src/views/Input.js
@@ -1,9 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
+import Validate from '../utils/Validate';
 
 /**
  * `@woowacourse/mission-utils`에서 제공하는 `Console` API를 사용하여 사용자의 입력을 받는 클래스
  */
 export default class Input {
+  constructor() {
+    this.validate = new Validate();
+  }
+
   /**
    * 사용자 입력값을 받아 구분자로 나누고 결괏값을 검증하는 메서드
    * @returns 자동차 이름 배열을 반환한다.
@@ -12,10 +17,11 @@ export default class Input {
     try {
       const MESSAGE = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
       const nameInput = await Console.readLineAsync(MESSAGE);
+      // 입력값을 쉼표를 기준으로 파싱하고 좌우 공백을 삭제한다.
       const nameArray = nameInput.split(',').map(name => name.trim());
-      
-      // Validate 한다
-
+      // 파싱한 입력값에 대한 검증 함수를 호출한다. 실패시 에러를 반환한다.
+      this.validate.validateNameArray(nameArray);
+      // 정상 입력값을 호출하고 결과를 반환한다.
       Console.print(nameArray);
       return nameArray;
     } catch (error) {
@@ -30,10 +36,12 @@ export default class Input {
   async readRounds() {
     try {
       const MESSAGE = '시도할 횟수는 몇 회인가요?';
-      const rounds = await Console.readLineAsync(MESSAGE);
-      
-      // Validate 한다
-
+      const roundsInput = await Console.readLineAsync(MESSAGE);
+      // 문자열로 읽은 숫자를 정수로 형변환한다.
+      const rounds = parseInt(roundsInput, 10);
+      // 변환한 입력값에 대한 검증 함수를 호출한다. 실패시 에러를 반환한다.
+      this.validate.validateRounds(rounds);
+      // 정상 입력값을 호출하고 결과를 반환한다.
       Console.print(rounds);
       return rounds;
     } catch (error) {
