eef9ed34d432ae5e1631cadb22faa6597f10bc7b
changuikim <cu.kim.dev@gmail.com>
2024-04-22 06:10:29 +0900

    feature : 레이싱카 과제 초기화
    - 레이싱카 과제의 초기 상태를 zip 형태로 가져와서 설정하였다.
    - App.js에 pseudo code를 작성하였다.

---
+++ week4-javascript-racing/src/App.js
@@ -0,0 +1,16 @@
+class App {
+  async play() {
+    // // pseudo code
+    // // 1 입력
+    // const car_array = RacingCarInputView.getRacingCarInput();
+    // const round_number = RacingCarInputView.getRoundInput();
+    // // 2 경기
+    // const racing = new Racing(car_array, round_number);
+    // const winner = racing.race();
+    // const output = new Output();
+    // // 3 출력
+    // output.winner(winner);
+  }
+}
+
+export default App;
